953086655eebf4e01ca5544d413cc7c04ff030ff
Herb Stahl <ghstahl@gmail.com>
2019-09-28 09:38:15 -0700

    missing

---
+++ src/Utils/EnvironmentHelperExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Utils
+{
+    public static class EnvironmentHelperExtensions
+    {
+        public static (bool exists, bool value) GetEnvironmentVariable(this string name,bool defaultValue)
+        {
+            var env = Environment.GetEnvironmentVariable(name);
+            if (env == null) return (false,defaultValue);
+            return (true,bool.Parse(env));
+        }
+        public static (bool exists, string value) GetEnvironmentVariable(this string name, string defaultValue)
+        {
+            var env = Environment.GetEnvironmentVariable(name);
+            if (env == null) return (false, defaultValue);
+            return (true, env);
+        }
+        public static (bool exists, int value) GetEnvironmentVariable(this string name, int defaultValue)
+        {
+            var env = Environment.GetEnvironmentVariable(name);
+            if (env == null) return (false, defaultValue);
+            return (true, int.Parse(env));
+        }
+    }
+}
